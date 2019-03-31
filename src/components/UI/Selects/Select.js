df247e73ef0bfdfd6acd0f09f722888977377f36
Luis Puentes <luispuentes@Luiss-MacBook-Pro.local>
2019-03-31 16:07:09 +0700

    Styling Child Card: Mobile Design

--- src/components/UI/Selects/Select.js
+++ src/components/UI/Selects/Select.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Select = (props) => {
     return (
         <select className="select" defaultValue="have-to-select" onChange={props.onChange}>
-            <option value="have-to-select">Please select Language you want</option>
+            <option value="have-to-select">..Please choose one topic..</option>
             {props.options.map((option, index) => {
                 return (
                     <option key={index} value={option}>{option}</option>

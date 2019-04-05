2ae33dd1e9f1ee70f9b87843d3f1f31ca2dd0ed3
Luis <luispuentesvega@gmail.com>
2019-04-05 22:46:40 +0700

    Add Loading...

--- src/components/TopicBlock/TopicList.js
+++ src/components/TopicBlock/TopicList.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import TopicItem from './TopicItem'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const TopicList = props => {
     return (
         <div className="child">
             <h1 className="child__title">LIST</h1>
             <div className="caption">
-                {Object.values(props.topics).map((topic, idx) => {
-                    return (
-                        <TopicItem
-                            key={idx}
-                            id={topic.id}
-                            name={topic.name}
-                            onDelete={props.onDelete}
-                        />
-                    )
-                })}
+                {props.loading ? (
+                    <Spinner />
+                ) : (
+                    Object.values(props.topics).map((topic, idx) => {
+                        return (
+                            <TopicItem
+                                key={idx}
+                                id={topic.id}
+                                name={topic.name}
+                                onDelete={props.onDelete}
+                            />
+                        )
+                    })
+                )}
             </div>
         </div>
     )
